0ba672277133d0f29cee53cbd6f8bbc72e9b35ac
Harold Allen <hmallen.cb@gmail.com>
2024-09-01 01:03:39 +0100

    fix

--- web/src/components/Music.jsx
+++ web/src/components/Music.jsx
@@ -4,6 +4,6 @@ export default function Music() {
 
     return <div className="section music">
         <p className="title">Now playing</p>
-        <iframe src="http://www.youtube.com/embed/uRI0_G9RIDw?list=PLG4z8Js-3FRQvaT-qV6XdCCvJ-YXfIcL9"></iframe>
+        <iframe src="http://www.youtube.com/embed/YdRjNeu11Ww?list=PLG4z8Js-3FRQvaT-qV6XdCCvJ-YXfIcL9"></iframe>
     </div>;
 }
